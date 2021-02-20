c53aac05b073e28900c62557e1cae7fefa4a5c1d
svbnet <joe@svbnet.co>
2021-02-20 19:39:19 +1300

    Fix media path variable

--- context.py
+++ context.py
@@ -1,10 +1,15 @@
 import os, os.path
 import sqlite3
 
+
+def relativize(filename):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+
+
 con = None
-media_dir = './media'
-template_path = os.getenv('TEMPLATE', os.path.join(os.path.dirname(os.path.abspath(__file__)), 'template_new.html'))
-db_path = os.getenv('DB', os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tweets.db'))
+media_dir = os.getenv('MEDIA_DIR', relativize('media'))
+template_path = os.getenv('TEMPLATE', relativize('template_new.html'))
+db_path = os.getenv('DB', relativize('tweets.db'))
 
 
 def open_db():
